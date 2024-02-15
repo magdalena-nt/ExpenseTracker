57ce60b8e4319dc62567618db718961200394c53
Magdalena <magdalena.nitecka99@gmail.com>
2024-02-15 10:25:50 +0100

    init

---
+++ expense-tracker.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace expense_tracker.web.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
